762a0c9e9c0fb427d4a62f6ecfa7f3b0610276f3
Adarsh Manickam <adarsh.manickam@vaimo.com>
2021-10-26 13:00:42 +0530

    Added dev mode 2FA disable

--- Plugin/BypassTwoFactorAuth.php
+++ Plugin/BypassTwoFactorAuth.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace MarkShust\DisableTwoFactorAuth\Plugin;
 
 use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\App\State;
 use Magento\TwoFactorAuth\Model\TfaSession;
 
 /**
@@ -13,18 +14,25 @@ use Magento\TwoFactorAuth\Model\TfaSession;
 class BypassTwoFactorAuth
 {
     const XML_PATH_CONFIG_ENABLE = 'twofactorauth/general/enable';
+    const XML_PATH_CONFIG_DISABLE_DEV_2FA = 'twofactorauth/general/disable_developer_auth';
 
     /** @var ScopeConfigInterface */
     private $scopeConfig;
 
+    /** @var State */
+    private $appState;
+
     /**
      * BypassTwoFactorAuth constructor.
      * @param ScopeConfigInterface $scopeConfig
+     * @param State $appState
      */
     public function __construct(
-        ScopeConfigInterface $scopeConfig
+        ScopeConfigInterface $scopeConfig,
+        State $appState
     ) {
         $this->scopeConfig = $scopeConfig;
+        $this->appState = $appState;
     }
 
     /**
@@ -33,6 +41,7 @@ class BypassTwoFactorAuth
      *
      * If 2FA is enabled, return the original result.
      * If 2FA is disabled, always return true so all requests bypass 2FA.
+     * If 2FA is disabled only for developer mode, return true when in developer mode to bypass 2FA.
      *
      * NOTE: Always keep 2FA enabled within production environments for security purposes.
      *
@@ -44,8 +53,17 @@ class BypassTwoFactorAuth
         TfaSession $subject,
         $result
     ): bool {
-        return $this->scopeConfig->isSetFlag(self::XML_PATH_CONFIG_ENABLE)
-            ? $result
-            : true;
+        // 2FA is always disabled.
+        if (!$this->scopeConfig->isSetFlag(self::XML_PATH_CONFIG_ENABLE)) {
+            return true;
+        }
+
+        // 2FA is disabled only for developer mode.
+        if ($this->scopeConfig->isSetFlag(self::XML_PATH_CONFIG_DISABLE_DEV_2FA)
+            && $this->appState->getMode() == State::MODE_DEVELOPER) {
+            return true;
+        }
+
+        return $result;
     }
 }
